4e34b83e0d3fb5b31a6a835e17c9a543bcecf74f
unknown <roopeshb13@gmail.com>
2022-07-23 17:41:34 +0530

    added some defaults

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/brownie'),
+      dir: require('path').join(__dirname, './coverage/angularnexwordcloud'),
       subdir: '.',
       reporters: [
         { type: 'html' },
